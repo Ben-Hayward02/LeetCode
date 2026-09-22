9b7e7c3ae62dac387bac8f93c7b2da89280f4e96
Ben Hayward <140420036+Ben-Hayward02@users.noreply.github.com>
2026-09-22 11:31:51 +0100

    Time: 341 ms (37.48%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -4,5 +4,8 @@ SELECT
     x
     , y
     , z
-    , IF(x + y > z AND x + z > y AND y + z > x, 'Yes', 'No') AS triangle
+    , CASE
+        WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
+        ELSE 'No' 
+        END AS triangle
 FROM Triangle;
